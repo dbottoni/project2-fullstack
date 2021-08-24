bfef6ab0df6de41ff0ac0bfa752dbc20f5401f34
Dana Bottoni <danabottoni@gmail.com>
2021-08-24 12:49:47 -0400

    begin route definition

---
+++ controllers/api/project-routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { Project } = require('../../models');
+
+
+
+module.exports = router;
+
---
+++ controllers/api/tasks-routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { Tasks } = require('../../models');
+
+
+
+module.exports = router;
+
