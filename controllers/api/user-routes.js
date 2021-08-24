55256f0669f7957a510ad0cb26d74d4b49503a7f
Dana Bottoni <danabottoni@gmail.com>
2021-08-24 13:09:19 -0400

    completed server.js file

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -46,6 +46,27 @@ router.get('/:id', (req,res) => {
   });
 });
 
+router.post('/', (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+  .then (dbUserData => {
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json(dbUserData);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
 
 
 module.exports = router;
