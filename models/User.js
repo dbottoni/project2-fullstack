55ec7b810594bb3aeaa5ccfd55fe06d18c0f3477
Dana Bottoni <danabottoni@gmail.com>
2021-08-23 20:36:30 -0400

    name db and more model setup

--- models/User.js
+++ models/User.js
@@ -2,15 +2,12 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-// create our User model
 class User extends Model {
-  // set up method to run on instance data (per user) to check password
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
-// create fields/columns for User model
 User.init(
   {
     id: {
