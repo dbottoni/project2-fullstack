851d01291640bc4baa71db2d676bc9ed7ed4bf1f
Dana Bottoni <danabottoni@gmail.com>
2021-08-26 13:00:20 -0400

    add delete route to tasks and update and delete route to user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -67,6 +67,45 @@ router.post('/', (req, res) => {
   });
 });
 
+router.put('/:id', (req,res) => {
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if(!dbUserData) {
+      res.status(404).json({ message: 'No user found with this ID' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 
 
 module.exports = router;
